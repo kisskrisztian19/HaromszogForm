ba829fec4657a42456b3ae6e79e4b894c27071b9
krisztián kiss <kisskrisztiannnnn@hotmail.com>
2020-10-05 12:38:07 +0200

    Add project files.

---
+++ Haromszogek/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Haromszogek
+{
+    public partial class frmfo : Form
+    {
+        private int aOldal, bOldal, cOldal;
+
+        public frmfo()
+        {
+            aOldal = 0;
+            bOldal = 0;
+            cOldal = 0;
+            InitializeComponent();
+            tbAoldal.Text = aOldal.ToString();
+            tbBoldal.Text = bOldal.ToString();
+            tbColdal.Text = cOldal.ToString();
+            lbHaromszogLista.Items.Clear();
+        }
+
+        private void btnTorol_Click(object sender, EventArgs e)
+        {
+            if (lbHaromszogLista.Items.Count > 0)
+            {
+                lbHaromszogLista.Items.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Nincs mit törölni.", "Hiba");
+            }
+        }
+
+        private void btnKilepes_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnSzamol_Click(object sender, EventArgs e)
+        {
+            aOldal = int.Parse(tbAoldal.Text);
+            bOldal = int.Parse(tbBoldal.Text);
+            cOldal = int.Parse(tbColdal.Text);
+
+
+
+            if (aOldal == 0 || bOldal == 0 || cOldal == 0)
+            {
+                MessageBox.Show("A háromszög oldala nem lehet 0", "Hiba", 
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                var h = new Haromszog(aOldal, bOldal, cOldal);
+
+                List<string> adatok = h.AdatokSzoveg();
+
+                foreach (var a in adatok)
+                {
+                    lbHaromszogLista.Items.Add(a);
+                }
+
+            }
+            
+        }
+    }
+}
---
+++ Haromszogek/Haromszog.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Haromszogek
+{
+    internal class Haromszog
+    {
+        private int aOldal;
+        private int bOldal;
+        private int cOldal;
+
+        public double terulet { get; private set; }
+        public double kerulet { get; private set; }
+
+        public bool szerkesztheto { get; private set; }
+
+        private void Szerk()
+        {
+            if ((aOldal + bOldal) > cOldal && 
+                (bOldal + cOldal) > aOldal && 
+                (aOldal + cOldal) > bOldal)
+            {
+                szerkesztheto = true;
+                terulet = teruletSzamitas();
+                kerulet = keruletSzamitas();
+            }
+            else
+            {
+                szerkesztheto = false;
+                terulet = 0;
+                kerulet = 0;
+            }
+        }
+
+        private double teruletSzamitas()
+        {
+            double s = (aOldal + bOldal + cOldal) / 2;
+            return Math.Sqrt(s * (s - aOldal) * (s - bOldal) * (s - cOldal)); 
+        }
+
+        private double keruletSzamitas()
+        {
+            return aOldal + bOldal + cOldal;
+        }
+
+        public Haromszog(int aOldal, int bOldal, int cOldal)
+        {
+            this.aOldal = aOldal;
+            this.bOldal = bOldal;
+            this.cOldal = cOldal;
+            Szerk();
+        }
+
+        public List<string> AdatokSzoveg()
+        {
+            List<string> adatok = new List<string>();
+            adatok.Add($"A:{aOldal} - B:{bOldal} - C:{cOldal}");
+           
+            if (szerkesztheto)
+            {
+                adatok.Add($"\nKerület: {kerulet} - Terület: {terulet}");
+            }
+            else
+            {
+                adatok.Add("Nem szerkeszthető.");
+            }
+            return adatok;
+        }
+    }
+}
